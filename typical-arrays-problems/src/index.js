880ad593cf3fce948138d097174474f5cfb23a53
Denis <crippton@gmail.com>
2020-10-03 20:10:04 +0300

    init: start typical-arrays-problems

---
+++ typical-arrays-problems/src/index.js
@@ -0,0 +1,13 @@
+
+exports.min = function min (array) {
+  let result = 0;
+  return result;
+}
+
+exports.max = function max (array) {
+  return 0;
+}
+
+exports.avg = function avg (array) {
+  return 0;
+}
